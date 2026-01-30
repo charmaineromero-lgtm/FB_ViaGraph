9f50828c5703ffddd885898d94a58edbd42af066
CHARMAINE HAZEL ROMERO <charmainehazel.romero@g.msuiit.edu.ph>
2026-01-30 04:03:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1994613203-314b3",
+  "appId": "1:30758033879:web:fc82b0b5fab94b21321392",
+  "apiKey": "AIzaSyD1VPz1UkhS9qeYKvF-hjbUgLZW19EvetI",
+  "authDomain": "studio-1994613203-314b3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "30758033879"
+};
